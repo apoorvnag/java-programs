cf6e3779c16bfe7a3c15ef7071f18cf28ee417f1
kiranjha <55039934+kiranjha@users.noreply.github.com>
2021-09-30 21:20:24 +0530

    Update SimpleApplet.java

--- AppletDemo/src/SimpleApplet.java
+++ AppletDemo/src/SimpleApplet.java
@@ -3,7 +3,7 @@ import java.applet.Applet;
 public class SimpleApplet extends Applet{
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
+		
 		super.start();
 		System.out.println("Hello");
 	}
